0cab5b0203708644e9ab89f781d596448e9adf53
Sat30 <satishhuddar03@gmail.com>
2024-11-07 10:27:06 +0530

    sample from the final states

--- sub_task_1.py
+++ sub_task_1.py
@@ -73,6 +73,31 @@ class My_Quantum_circuit1():
         curr_state = curr_state.reshape((2,) * self.num_qubits)
         curr_state = np.moveaxis(curr_state, [0, 1], [control_qubit, target_qubit])
         self.state = curr_state.flatten()
+     
+    def measure(self, qubitindex : int, shots : int = 1):
+        self.check_out_of_index(qubitindex)
+        counts = {}
+        probs = np.zeros(2)
+        probs[1] = 0
+        for i in range(len(self.state)):
+            if (i >> qubitindex) & 1 == 1:
+                probs[1] += (self.state[i] * self.state[i] )
+        probs[0] = 1 - probs[1]
+        last_measured_state = 0
+        for i in range(shots):
+            curr = random.random()
+            if curr < probs[0]:
+                counts[0] = counts.get(0, 0) + 1
+                last_measured_state = 0
+            else :
+                counts[1] = counts.get(1, 0) + 1
+                last_measured_state = 1
 
-        
-
+        amplitudes = np.sqrt(probs)
+        for i in range(len(self.state)):
+            curri = i
+            if (i >> qubitindex) & 1 == last_measured_state:
+                self.state[i] /= amplitudes[last_measured_state]
+            else :
+                self.state[i] = 0
+        return (counts, last_measured_state)
